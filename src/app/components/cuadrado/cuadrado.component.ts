a7367edba291a3f189d557f72a0a0ee398c470a0
datecasp <ciudadano.fake@gmail.com>
2023-10-10 23:06:29 +0200

    pintar continuo 1

--- src/app/components/cuadrado/cuadrado.component.ts
+++ src/app/components/cuadrado/cuadrado.component.ts
@@ -8,11 +8,26 @@ import { PintarService } from 'src/app/services/pintar.service';
 })
 export class CuadradoComponent {
   @Input() color: string = 'transparent'; // Color inicial transparente
-
+btnPulsado : boolean = false;
   constructor(private pintarService: PintarService) { }
 
-  cambiarColor() {
+  public EmpezarPintar() {
     // Cambia el color cuando se hace clic en el cuadrado
     this.color = this.pintarService.colorElegido;
+    this.cambiaPulsador();
+  }
+
+  public cambiaPulsador(): void {
+    this.pintarService.btnPulsado = !this.pintarService.btnPulsado;
+  }
+
+  public Pintar(): void {
+    if (this.pintarService.btnPulsado) {
+      this.color = this.pintarService.colorElegido;
+    }
+  }
+
+  public PararPintar(): void {
+    this.pintarService.btnPulsado = false;
   }
 }
