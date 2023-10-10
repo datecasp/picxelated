cc4b08c95b22c6493cf2067368cc156d3d851d86
datecasp <ciudadano.fake@gmail.com>
2023-10-10 23:02:28 +0200

    pintado continuo

--- src/app/components/cuadrado/cuadrado.component.ts
+++ src/app/components/cuadrado/cuadrado.component.ts
@@ -4,15 +4,30 @@ import { PintarService } from 'src/app/services/pintar.service';
 @Component({
   selector: 'app-cuadrado',
   templateUrl: './cuadrado.component.html',
-  styleUrls: ['./cuadrado.component.css']
+  styleUrls: ['./cuadrado.component.css'],
 })
 export class CuadradoComponent {
   @Input() color: string = 'transparent'; // Color inicial transparente
 
-  constructor(private pintarService: PintarService) { }
+  constructor(private pintarService: PintarService) {}
 
-  cambiarColor() {
+  public EmpezarPintar() {
     // Cambia el color cuando se hace clic en el cuadrado
     this.color = this.pintarService.colorElegido;
+    this.cambiaPulsador();
+  }
+
+  public cambiaPulsador(): void {
+    this.pintarService.btnPulsado = !this.pintarService.btnPulsado;
+  }
+
+  public Pintar(): void {
+    if (this.pintarService.btnPulsado) {
+      this.color = this.pintarService.colorElegido;
+    }
+  }
+
+  public PararPintar(): void {
+    this.pintarService.btnPulsado = false;
   }
 }
--- src/app/services/pintar.service.ts
+++ src/app/services/pintar.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PintarService {
-
+  public btnPulsado: boolean = false;
   public colorElegido : string = "";
 
   constructor() { }
